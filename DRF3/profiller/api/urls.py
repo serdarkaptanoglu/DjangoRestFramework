a806601d400d0be1e98694ffd935b301aaa958b6
serdarkaptanoglu <serdarkaptanog57@gmail.com>
2024-12-17 01:57:53 +0300

    added Viewsets and Routers

--- DRF3/profiller/api/urls.py
+++ DRF3/profiller/api/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
-from profiller.api import views
+from django.urls import path,include
+from profiller.api.views import ProfilViewSet, ProfilDurumViewSet, ProfilFotoUpdateView
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'profiller', ProfilViewSet),
+router.register(r'durumlar', ProfilDurumViewSet, basename='durum'),
 
 urlpatterns = [
-    path('kullanici-profilleri/', views.ProfilList.as_view(), name='profiller'),
+    path('', include(router.urls)),
+    path('profil_foto/', ProfilFotoUpdateView.as_view(), name='profil-foto'),
 ]
--- DRF3/profiller/api/views.py
+++ DRF3/profiller/api/views.py
@@ -1,10 +1,43 @@
-from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
-from profiller.models import Profil
-from profiller.api.serializers import ProfilSerializer
+from profiller.models import Profil, ProfilDurum
+from profiller.api.serializers import ProfilSerializer, ProfilDurumSerializer, ProfilFotoSerializer
+from rest_framework.viewsets import GenericViewSet, ModelViewSet
+from rest_framework import mixins, generics
+from profiller.api.permissions import KendiProfilOrReadOnly, DurumSahibiOrReadOnly
+from rest_framework.filters import SearchFilter
 
 
-class ProfilList(generics.ListAPIView):
+class ProfilViewSet(mixins.ListModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.RetrieveModelMixin,
+                    GenericViewSet):
     queryset = Profil.objects.all()
     serializer_class = ProfilSerializer
+    permission_classes = [IsAuthenticated, KendiProfilOrReadOnly]
+    filter_backends = [SearchFilter]
+    search_fields = ['sehir']
+
+
+class ProfilDurumViewSet(ModelViewSet):
+    serializer_class = ProfilDurumSerializer
+    permission_classes = [IsAuthenticated, DurumSahibiOrReadOnly]
+
+    def get_queryset(self):
+        queryset = ProfilDurum.objects.all()
+        username = self.request.query_params.get('username', None)
+        if username is not None:
+            queryset = queryset.filter(user_profil__user__username=username)
+        return queryset
+
+    def perform_create(self, serializer):
+        user_profil = self.request.user.profil
+        serializer.save(user_profil=user_profil)
+
+
+class ProfilFotoUpdateView(generics.UpdateAPIView):
+    serializer_class = ProfilFotoSerializer
     permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        profil_nesnesi = self.request.user.profil
+        return profil_nesnesi
